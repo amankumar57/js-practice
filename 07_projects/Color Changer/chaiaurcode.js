04cb2615db6588ed12f4a47ec39d48d423933b6e
amank0507 <amankumar5799@gmail.com>
2025-01-16 21:56:34 +0530

    projects upto events

--- 07_projects/Color Changer/chaiaurcode.js
+++ 07_projects/Color Changer/chaiaurcode.js
@@ -15,5 +15,9 @@ buttons.forEach(function(button){
         if(e.target.id === 'yellow'){
             body.style.backgroundColor = e.target.id;
         }
-    })
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+
+    });
 });
